d4667d869ae85547f11c91e7222fd0e49ea27a71
Yuchao Zhu <z5362398@ad.unsw.edu.au>
2021-09-28 12:52:24 +1000

    Testing Iteration 1

--- src/data_store.py
+++ src/data_store.py
@@ -23,15 +23,63 @@ Example usage:
     print(store) # Prints { 'names': ['Emily', 'Hayden', 'Jake', 'Nick'] }
     data_store.set(store)
 '''
+import re
 
 ## YOU SHOULD MODIFY THIS OBJECT BELOW
 initial_object = {
-    'email': "",
-    'password': "",
-    'name_first': "",
-    'name_last': "",
-
+    'user': [    
+    ]
 }
+
+def add_user(email, password, name_first, name_last):
+    
+    store = data_store.get()                                   # gets user data from initial object
+    u_id = len(name_first) +len(name_last) +len(email) + randrange(100000)
+    user = make_user(email, password, name_first, name_last, u_id)   # Remember to Add more fields
+#    data_store.set(store)
+    store['user'].append(user)
+    return user
+
+def make_user(email, password, name_first, name_last, u_id):
+
+    return {
+            'u_id': u_id,
+            'email': email,  
+            'password': password, 
+            'name_first': name_first,
+            'name_last': name_last, 
+            'handle_str': create_handle(name_first, name_last),
+    }
+
+def create_handle(first_name, last_name):
+    
+    sample_handle = first_name + last_name                       # Concatenation of first and last name
+    sample_handle = sample_handle.lower()                        # lowercased string
+
+    if handle_check(sample_handle):                              # If passes handle_check 
+        sample_handle = sample_handle + str(randrange(25000))    # Generate unique handle based on random generator
+
+    if len(sample_handle) > 20:                                  # Ensure handle size less than 20 chars
+        sample_handle = sample_handle[0:20]
+
+    return sample_handle
+
+def handle_check(handle_str):                                   # Function to check handle
+#    data = get_user_store()
+    data = data_store.get()
+    for user in data['users']:
+        if user['handle_str'] == handle_str:
+            return True
+    return False
+
+def email_check(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if (re.fullmatch(regex, email)):
+        return True
+    else:
+        return False
+
+
 ## YOU SHOULD MODIFY THIS OBJECT ABOVE
 
 class Datastore:
--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,11 +1,7 @@
-import re
 import pytest
 
 from src.error import InputError
 from src.auth import auth_register_v1
-
-
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 
 #def test_check(email):
@@ -19,7 +15,7 @@ regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 def test_auth_register_email_invalid():
     with pytest.raises(InputError):
         assert auth_register_v1("yuchaocool.com", "password", "yuchao", "zhu")                  # Invalid email- no @
-#        assert auth_register_v1("yuchaocool@", "password", "yuchao", "zhu")                    # Same user      
+#        assert auth_register_v1("yuchaocool@", "password", "yuchao", "zhu")                    # Same user email_repeat_check()      
 def test_auth_register_password_invalid():
     with pytest.raises(InputError):
         assert auth_register_v1("jackysydau@gmail.com", "pie", "yuchao", "zhu")                 # Password less than 6 characters
