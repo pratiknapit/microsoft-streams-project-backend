728746fb8303378db7e6340890f9106f532a2fe1
z5311922 <z5311922@cse.unsw.edu.au>
2021-10-01 19:28:45 +1000

    Channels test - still need to be slightly modified

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -18,40 +18,65 @@ def test_channels_create_invalid_user_id(): #user id is invalid
 def test_channels_create_min_invalid_name(): 
     clear_v1()
     with pytest.raises(InputError): #length of name is less than 1 
-        channels_create_v1("pratiknapit", "", "is_public")
+        channels_create_v1("12345", "", True)
 
 def test_channels_create_max_invalid_name(): 
     clear_v1()
     with pytest.raises(InputError): #length of name is more than 20
-        channels_create_v1("pratiknapit", "channel24istheworstchannelEver", "is_public")
+        channels_create_v1("12345", "channel24istheworstchannelEver", False)
 
-"""
-def test_channels_create_invalid_ispublic(): 
+def test_channels_create_wrong_public_input():
     clear_v1()
-    with pytest.raises(InputError): #is_public is invalid --> needs to be either TRUE or FALSE
-        channels_create_v1("pratiknapit", "BestChannel", "maybe")
-"""
+    with pytest.raises(InputError):
+        channels_create_v1("12345", "Sports", "Not private")
 
 #The tests below actually check if the function works and does what we want it to do.
 #That is creating the channel and adding to the data base.
 
 def test_channels_create():
     clear_v1()
-    channel_return = channels_create_v1("ConorMcgregor", "School", "yes")
-    channel_id1 = channel_return['channel_id'] #this will just return the channel id
-    
-    channel_list = channels_list_v1("ConorMcgregor") #this should return a list of channels
+    channel_id = channels_create_v1("12345", "School", True)  #this will just return the channel id
+    channel_list = channels_list_v1("12345") #this should return a list of channels
     for channel in channel_list['channels']:
-        if channel['name'] == "School":
-            channel_id2 = channel['channel_id']
+        if channel['channel_id'] == channel_id['channel_id'] and channel['name'] == "School":
+            result = True
+
+    assert (result == True)
+            
+
+def test_multiple_channels_create():
+    clear_v1()
+    channel_1 = channels_create_v1("12345", "School", True) #this shud return
+    channel_2 = channels_create_v1("12345678", "Play", True)
+    channel_3 = channels_create_v1("12345", "Tutoring", True) #this shud return
+    channel_4 = channels_create_v1("12345", "Yellow", False) #this shud return
+
+    channel_list = channels_list_v1("12345")
+    
+    result = 0
+    for channel in channel_list['channels']:
+        if channel['channel_id'] == channel_1['channel_id'] and channel['name'] == "School":
+            result = result + 1
+        elif channel['channel_id'] == channel_3['channel_id'] and channel['name'] == "Tutoring":
+            result = result + 1
+        elif channel['channel_id'] == channel_4['channel_id'] and channel['name'] == "Yellow":
+            result = result + 1
         else:
-            channel_id2 = 100
-    
-    assert channel_id1 == channel_id2
-            
-    
-            
-    
+            pass
+    #result should return 3 
+    assert (result == 3)
+        
+#testing if channels create works using channel_list_all
+
+
+#testing if channels create works using channel_join
+
+
+#testing if channels create works using channels_invite 
+
+#testing if channels create works using channel details 
+
+
 
 
 
