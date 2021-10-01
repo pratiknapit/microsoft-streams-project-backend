b5f9cea7376ec8bef3439acc74c287b558020926
z5362386 <z5362386@cse.unsw.edu.au>
2021-10-01 16:44:27 +1000

    added tests for channel_details

--- tests/channel_details_test.py
+++ tests/channel_details_test.py
@@ -1,30 +1,30 @@
 import pytest
-from auth import auth_register_v1 
-from error import InputError, AccessError  
-from channel import channel_details_v1 
-from channels import channels_create_v1
+from src.auth import auth_register_v1 
+from src.error import InputError, AccessError  
+from src.channel import channel_details_v1 
+from src.channels import channels_create_v1
 
 #Create a dummy user 
 steven_dummy = auth_register_v1('stevenchen@gmail.com','workingpassword','steven','chen')
 steven = channels_create_v1(steven_dummy['auth_user_id'], 'steven_channel', True)
 jacky_dummy = auth_register_v1('jackyzhu@gmail.com','passwordworking','jacky','zhu')
-jacky = channel_create_v1(jacky_dummy['auth_user_id'],'jacky_channel',True)
+jacky = channels_create_v1(jacky_dummy['auth_user_id'],'jacky_channel',True)
 
 def test_invalid_channel_details():
-    with pytest.raises(AccessError)
+    with pytest.raises(AccessError):
         channel_details_v1(steven_dummy['auth_user_id'], jacky['channel_id'])
-    with pytest.raises(InputError)
+    with pytest.raises(InputError):
         channel_details_v1(steven_dummy['auth_user_id'], 543210)
 
 def test_valid_channel_details():
     assert(channel_details(steven_dummy['auth_user_id'], steven_dummy['channel_id']) ==
     {
-        'name': 'steven_channel'
+        'name': 'steven_channel',
 
-        'owner_members': [{'name_first': steven_dummy['name_first']
-        'name_last': steven_dummy['name_last'], 'u_id': steven_dummy['u_id']}];
+        'owner_members': [{'name_first': steven_dummy['name_first'],
+        'name_last': steven_dummy['name_last'], 'u_id': steven_dummy['u_id']}],
 
-        'all_members': [{'name_first': steven_dummy['name_first']
-        'name_last': steven_dummy['name_last'], 'u_id': steven_dummy['u_id']}];
+        'all_members': [{'name_first': steven_dummy['name_first'],
+        'name_last': steven_dummy['name_last'], 'u_id': steven_dummy['u_id']}],
     }
     )
