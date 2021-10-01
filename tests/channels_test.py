26552a306a2cae1d975a8b8da6b1cbb962c33960
z5311922 <z5311922@cse.unsw.edu.au>
2021-10-01 22:56:07 +1000

    Channels create, list and list all tests.

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -1,42 +1,48 @@
 import pytest
 
+from src.auth import auth_register_v1
 from src.error import InputError
 from src.channels import channels_create_v1, channels_list_v1
+from src.auth import auth_register_v1
 from src.other import clear_v1
 
 #The first part of the testing is to test if the inputs are valid. 
 
 #we might have to check if the user id that is inputted is valid 
 
-"""
-def test_channels_create_invalid_user_id(): #user id is invalid
+dummy1 = auth_register_v1("dummyvariableone@gmail.com", "password", "pratik", "napit")
+dummy2 = auth_register_v1("dummyvariabletwo@gmail.com", "password", "conor", "mishra")
+dummy1_id = dummy1['auth_user_id']
+dummy2_id = dummy2['auth_user_id']
+
+#Testing Errors
+def test_user_is_valid(): 
     clear_v1()
-    with pytest.raises(InputError): #invalid auth user id. 
-        channels_create_v1("kjeklfjkfgksjfkdsajfasdkjfk", "name", "is_public")
-"""
-    
+    with pytest.raises(InputError): #length of name is less than 1 
+        channels_create_v1(12345, "games", True)
+
 def test_channels_create_min_invalid_name(): 
     clear_v1()
     with pytest.raises(InputError): #length of name is less than 1 
-        channels_create_v1(12345, "", True)
+        channels_create_v1(dummy1_id, "", True)
 
 def test_channels_create_max_invalid_name(): 
     clear_v1()
     with pytest.raises(InputError): #length of name is more than 20
-        channels_create_v1(12345, "channel24istheworstchannelEver", False)
+        channels_create_v1(dummy1_id, "channel24istheworstchannelEver", False)
 
 def test_channels_create_wrong_public_input():
     clear_v1()
     with pytest.raises(InputError):
-        channels_create_v1(12345, "Sports", "Not private")
+        channels_create_v1(dummy1_id, "Sports", "Not private")
 
 #The tests below actually check if the function works and does what we want it to do.
 #That is creating the channel and adding to the data base.
 
 def test_channels_create():
     clear_v1()
-    channel_id = channels_create_v1(12345, "School", True)  #this will just return the channel id
-    channel_list = channels_list_v1(12345) #this should return a list of channels
+    channel_id = channels_create_v1(dummy1_id, "School", True)  #this will just return the channel id
+    channel_list = channels_list_v1(dummy1_id) #this should return a list of channels
     for channel in channel_list['channels']:
         if channel['channel_id'] == channel_id['channel_id'] and channel['name'] == "School":
             result = True
@@ -46,12 +52,12 @@ def test_channels_create():
 
 def test_multiple_channels_create():
     clear_v1()
-    channel_1 = channels_create_v1(12345, "School", True) #this shud return
-    channel_2 = channels_create_v1(12345678, "Play", True)
-    channel_3 = channels_create_v1(12345, "Tutoring", True) #this shud return
-    channel_4 = channels_create_v1(12345, "Yellow", False) #this shud return
+    channel_1 = channels_create_v1(dummy1_id, "School", True) #this shud return
+    channel_2 = channels_create_v1(dummy2_id, "Play", True)
+    channel_3 = channels_create_v1(dummy1_id, "Tutoring", True) #this shud return
+    channel_4 = channels_create_v1(dummy1_id, "Yellow", False) #this shud return
 
-    channel_list = channels_list_v1(12345)
+    channel_list = channels_list_v1(dummy1_id)
     
     result = 0
     for channel in channel_list['channels']:
