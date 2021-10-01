433e56c9143e6619588a4d04c35063d4d018873a
z5311922 <z5311922@cse.unsw.edu.au>
2021-10-01 14:33:42 +1000

    Channels create tests that still needs to be improved.

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -4,14 +4,16 @@ from src.error import InputError
 from src.channels import channels_create_v1, channels_list_v1
 from src.other import clear_v1
 
-
+#The first part of the testing is to test if the inputs are valid. 
 
 #we might have to check if the user id that is inputted is valid 
 
+"""
 def test_channels_create_invalid_user_id(): #user id is invalid
     clear_v1()
     with pytest.raises(InputError): #invalid auth user id. 
         channels_create_v1("kjeklfjkfgksjfkdsajfasdkjfk", "name", "is_public")
+"""
 
 def test_channels_create_min_invalid_name(): 
     clear_v1()
@@ -30,19 +32,21 @@ def test_channels_create_invalid_ispublic():
         channels_create_v1("pratiknapit", "BestChannel", "maybe")
 """
 
-#the test below will tests if channel_create_v1 works and if the channel has been added to the data store
-def test_channels_create_works(): 
-    clear_v1()
-    channel_return = channels_create_v1("auth_user_id", "name", "is_public")
-    channel_id1 = channel_return['channel_id']
+#The tests below actually check if the function works and does what we want it to do.
+#That is creating the channel and adding to the data base.
 
-    channel_list = channels_list_v1("auth_user_id") #this should return a list of channels
-    for channel in channel_list:
-        if channel['owners'] == "auth_user_id":
+def test_channels_create():
+    clear_v1()
+    channel_return = channels_create_v1("ConorMcgregor", "School", "yes")
+    channel_id1 = channel_return['channel_id'] #this will just return the channel id
+    
+    channel_list = channels_list_v1("ConorMcgregor") #this should return a list of channels
+    for channel in channel_list['channels']:
+        if channel['name'] == "School":
             channel_id2 = channel['channel_id']
         else:
-            pass
-
+            channel_id2 = 100
+    
     assert channel_id1 == channel_id2
             
     
