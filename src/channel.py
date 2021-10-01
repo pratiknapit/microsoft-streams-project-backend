fec92a341e3939a53760398e66dccd30fb98fa01
Safwan Hossain <z5367376@ad.unsw.edu.au>
2021-10-01 23:12:33 +1000

    Implemented functions for channel_invite_v1

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,33 @@
+from src.error import InputError, AccessError
+from src.data_store import channel_id_check, user_id_check, check_if_user_is_channel_member
+from src.data_store import data_store
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
-    return {
-    }
+    # check if corect channel id otherwise return Input error (not valid channel)
+    if not channel_id_check(channel_id): 
+            raise InputError
+
+    # check if user id correct otherwise return Input Error (not valid  user id or user 
+    # already in channel)
+    if not user_id_check(u_id):
+            raise InputError
+
+    # check when channel id corect but user(who invited) not part of channel
+    if not check_if_user_is_channel_member(auth_user_id, channel_id):
+            raise AccessError
+
+    if auth_user_id == u_id:
+        raise AccessError
+    
+    data = data_store.get()
+    user = user_id_check(u_id)
+    for channel in data["channels"]:
+        if channel["channel_id"] == channel_id:
+            channel["all_members"].append({"u_id": user["u_id"], "name_first": user['name_first'], "name_last": user["name_last"]})
+    user['user_list_channel'].append(channel_id)
+    data_store.set(data)
+    return {}
+
 
 def channel_details_v1(auth_user_id, channel_id):
     return {
