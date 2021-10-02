d68e68cc5a29bc47a9273ab8a315da907c1f7a02
Safwan Hossain <z5367376@ad.unsw.edu.au>
2021-10-02 10:28:25 +1000

    made various changes to functions

--- src/channel.py
+++ src/channel.py
@@ -1,9 +1,37 @@
-
-
 from src.error import InputError, AccessError
+from src.data_store import channel_id_check, auth_user_id_check, check_if_user_is_channel_member
+from src.data_store import auth_user_id_check
+from src.data_store import data_store
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
-    return {
-    }
+    # check if corect channel id otherwise return Input error (not valid channel)
+    if not channel_id_check(channel_id): 
+            raise InputError
+
+    # check if user id correct otherwise return Input Error (not valid  user id or user 
+    # already in channel)
+    if not auth_user_id_check(u_id):
+            raise InputError
+
+    # check when channel id corect but user(who invited) not part of channel
+    if not check_if_user_is_channel_member(auth_user_id, channel_id):
+            raise AccessError
+
+
+    #check if user is inviting himself
+    if auth_user_id == u_id:
+        raise AccessError
+    
+
+    data = data_store.get()
+    user = auth_user_id_check(auth_user_id)
+    for channel in data["channels"]:
+        if channel["channel_id"] == channel_id:
+            channel["all_members"].append(user["auth_user_id"])
+    '''user['user_list_channel'].append(channel_id)'''
+    data_store.set(data)
+    return {}
+
 
 def channel_details_v1(auth_user_id, channel_id):
     return {
@@ -30,10 +58,13 @@ def channel_details_v1(auth_user_id, channel_id):
 
 def channel_messages_v1(auth_user_id, channel_id, start):
 
-    if channel_check(channel_id) == False:                          # Does not refer valid channel
+    if not auth_user_id_check(auth_user_id):   
+        raise AccessError                            # User not exist at all
+
+    if channel_id_check(channel_id) == False:                              # Channel does not exist
         raise InputError
 
-    if check_if_user_in_channel_member(, channel_id) == False: # Valid channel_id & auth_user_id not part of channel
+    if check_if_user_is_channel_member(auth_user_id, channel_id) == False: # auth_user_id is not a member of channel
         raise AccessError
 
     total_messages = 0   
@@ -63,14 +94,14 @@ def channel_messages_v1(auth_user_id, channel_id, start):
              
                 if counter >= start:
                   
-                    dict_to_app = {
-                        'message_id':message['message_id'],
-                        'u_id': message['user_id'],
+                    dict = {
+                        'message_id': message['message_id'],
+                        'u_id': message['auth_user_id'],
                         'message': message['message'],
                         'time_created': message['time_created'].replace(tzinfo=timezone.utc).timestamp(),
                         
                     }
-                    dict_finish['messages'].append(dict_to_app)    
+                    dict_finish['messages'].append(dict)    
                 counter = counter + 1
             if counter >= 50:
                 counter = -1
@@ -79,20 +110,6 @@ def channel_messages_v1(auth_user_id, channel_id, start):
     dict_finish['start'] = start
     dict_finish['end'] = counter
     return dict_finish
-
-
-#    return {
-#        'messages': [
-#            {
-#                'message_id': 1,
-#                'u_id': 1,
-#                'message': 'Hello world',
-#                'time_created': 1582426789,
-#            }
-#        ],
-#        'start': 0,
-#        'end': 50,
-#    }
 
 def channel_join_v1(auth_user_id, channel_id):
     return {
